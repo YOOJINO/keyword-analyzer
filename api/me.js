14a9636f24b9e8c49b876ff0a7d61d87ec709b03
YOOJINO <lawofattraction2027@gmail.com>
2026-05-01 01:10:05 +0900

    Create me.js

---
+++ api/me.js
@@ -0,0 +1,7 @@
+import { getAuthFromReq } from './_lib/auth.js';
+
+export default function handler(req, res) {
+  const user = getAuthFromReq(req);
+  if (!user) return res.status(401).json({ error: 'Unauthorized' });
+  return res.status(200).json({ user: { id: user.id, role: user.role } });
+}
